3d82ce0ef49822a465d5e4cd7139a21d897bf7b3
VASANTH04032004 <154589849+VASANTH04032004@users.noreply.github.com>
2024-09-06 13:43:36 +0530

    Vasanth/button type (#4)
    * Make index-based answer row clickable and update selected color:
    
    - Use `GestureDetector` to make the entire answer row clickable.
    - Change both checkbox and container colors to `Color(0xFF098EAB)` when selected.
    - Ensure consistent styling between index-based and radio button answers.
    - Add fade and scale animations for smooth user interaction.
    
    * Make index-based answer row clickable and update selected color:
    
    - Use `GestureDetector` to make the entire answer row clickable.
    - Change both checkbox and container colors to `Color(0xFF098EAB)` when selected.
    - Ensure consistent styling between index-based and radio button answers.
    - Add fade and scale animations for smooth user interaction.
    
    * fix: Reset checkbox state and progress animation when moving to the next question
    
    * Refactor: Replace null assertion operators with null-aware operators for safer code execution
    
    - Replaced `!` with `?` in various places to prevent potential null pointer exceptions.
    - Added null checks for `_quizCategory` and its properties throughout the `QuizScreen` class.
    - Updated logic to handle null cases more gracefully during the quiz flow.

--- lib/screen/quiz_screen.dart
+++ lib/screen/quiz_screen.dart
@@ -16,8 +16,8 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> with SingleTickerProviderStateMixin {
   int _currentQuestionIndex = 0;
   int _score = 0;
-  Answer? _selectedAnswer;
-  late QuizCategory? _quizCategory;
+  List<Answer> _selectedAnswers = [];
+  QuizCategory? _quizCategory;
   late AnimationController _animationController;
   late Animation<double> _progressAnimation;
   late double _progressValue;
@@ -38,32 +38,42 @@ class _QuizScreenState extends State<QuizScreen> with SingleTickerProviderStateM
   }
 
   void _nextQuestion() {
-    if (_selectedAnswer == _quizCategory?.questions[_currentQuestionIndex].correctAnswer) {
-      _score++;
-    }
+    setState(() {
+      // Validate and update the score
+      if (_quizCategory?.questions[_currentQuestionIndex].isIndexBased == true) {
+        if (_selectedAnswers.toSet().containsAll(_quizCategory?.questions[_currentQuestionIndex].correctAnswers ?? [])) {
+          _score += 10;
+        }
+      } else {
+        if (_selectedAnswers.isNotEmpty &&
+            _quizCategory?.questions[_currentQuestionIndex].correctAnswers.contains(_selectedAnswers.first) == true) {
+          _score += 10;
+        }
+      }
 
-    if (_currentQuestionIndex < (_quizCategory?.questions.length ?? 0) - 1) {
-      setState(() {
+      // Move to the next question if available
+      if (_currentQuestionIndex < (_quizCategory?.questions.length ?? 0) - 1) {
         _currentQuestionIndex++;
-        _selectedAnswer = null;
+        _selectedAnswers.clear(); // Clear selected answers for the new question
         _progressValue = (_currentQuestionIndex + 1) / (_quizCategory?.questions.length ?? 1);
-        _progressAnimation = Tween<double>(begin: _progressAnimation.value, end: _progressValue).animate(
+        _progressAnimation = Tween<double>(begin: 0, end: _progressValue).animate(
           CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
         );
-        _animationController.forward(from: 0);
-      });
-    } else {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ResultScreen(
-            score: _score,
-            total: _quizCategory?.questions.length ?? 0,
-            userName: widget.userName,
+        _animationController.forward(from: 0); // Reset the animation for progress bar
+      } else {
+        // Navigate to the result screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResultScreen(
+              score: _score,
+              total: _quizCategory?.questions.length ?? 0,
+              userName: widget.userName,
+            ),
           ),
-        ),
-      );
-    }
+        );
+      }
+    });
   }
 
   @override
@@ -86,114 +96,154 @@ class _QuizScreenState extends State<QuizScreen> with SingleTickerProviderStateM
           },
         ),
       ),
-      body: Container(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Center(
-                  child: AnimatedBuilder(
-                    animation: _animationController,
-                    builder: (context, child) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: LinearProgressIndicator(
-                          value: _progressAnimation.value,
-                          minHeight: 16,
-                          backgroundColor: Colors.transparent,
-                          color: Color(0xFF098EAB),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      );
-                    },
-                  ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: LinearProgressIndicator(
+                        value: _progressAnimation.value,
+                        minHeight: 16,
+                        backgroundColor: Colors.transparent,
+                        color: Color(0xFF098EAB),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    );
+                  },
                 ),
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      FadeTransition(
-                        opacity: _animationController.drive(
-                          Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: Curves.easeInOut)),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 54.0),
-                          child: Text(
-                            _quizCategory!.questions[_currentQuestionIndex].question,
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeTransition(
+                      opacity: _animationController.drive(
+                        Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: Curves.easeInOut)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 54.0),
+                        child: Text(
+                          _quizCategory?.questions[_currentQuestionIndex].question ?? '',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                         ),
                       ),
-                      Expanded(
-                        child: ListView(
-                          children: _quizCategory!.questions[_currentQuestionIndex].answers.map<Widget>((answer) {
-                            bool isSelected = _selectedAnswer == answer;
-                            return FadeTransition(
-                              opacity: _animationController.drive(
-                                Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: Curves.easeInOut)),
-                              ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: _quizCategory?.questions[_currentQuestionIndex].answers.map<Widget>((answer) {
+                          bool isSelected = _selectedAnswers.contains(answer);
+                          return FadeTransition(
+                            opacity: _animationController.drive(
+                              Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: Curves.easeInOut)),
+                            ),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (_quizCategory?.questions[_currentQuestionIndex].isIndexBased == true) {
+                                    if (isSelected) {
+                                      _selectedAnswers.remove(answer);
+                                    } else {
+                                      _selectedAnswers.add(answer);
+                                    }
+                                  } else {
+                                    _selectedAnswers.clear();
+                                    _selectedAnswers.add(answer);
+                                  }
+                                });
+                              },
                               child: Container(
                                 margin: EdgeInsets.symmetric(vertical: 8),
                                 padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
                                 decoration: BoxDecoration(
+                                  color: isSelected ? Color(0xFF098EAB).withOpacity(0.1) : Colors.transparent,
                                   border: Border.all(
-                                    color: isSelected ? Colors.green : Color(0xFF098EAB),
+                                    color: isSelected ? Color(0xFF098EAB) : Colors.grey,
                                     width: 2,
                                   ),
                                   borderRadius: BorderRadius.circular(8),
                                 ),
-                                child: RadioListTile<Answer>(
-                                  title: Text(answer.answerText, style: TextStyle(fontSize: 18, color: Colors.black)),
-                                  value: answer,
-                                  groupValue: _selectedAnswer,
-                                  contentPadding: EdgeInsets.zero,
-                                  activeColor: Color(0xFF098EAB),
-                                  onChanged: (Answer? value) {
-                                    setState(() {
-                                      _selectedAnswer = value;
-                                    });
-                                  },
+                                child: Row(
+                                  children: [
+                                    if (_quizCategory?.questions[_currentQuestionIndex].isIndexBased == true)
+                                      Checkbox(
+                                        value: isSelected,
+                                        activeColor: Color(0xFF098EAB),
+                                        onChanged: (bool? value) {
+                                          setState(() {
+                                            if (value == true) {
+                                              _selectedAnswers.add(answer);
+                                            } else {
+                                              _selectedAnswers.remove(answer);
+                                            }
+                                          });
+                                        },
+                                      ),
+                                    if (_quizCategory?.questions[_currentQuestionIndex].isIndexBased != true)
+                                      Radio<Answer>(
+                                        value: answer,
+                                        groupValue: _selectedAnswers.isNotEmpty ? _selectedAnswers.first : null,
+                                        activeColor: Color(0xFF098EAB),
+                                        onChanged: (Answer? value) {
+                                          setState(() {
+                                            _selectedAnswers.clear();
+                                            if (value != null) {
+                                              _selectedAnswers.add(value);
+                                            }
+                                          });
+                                        },
+                                      ),
+                                    Expanded(
+                                      child: Text(
+                                        answer.answerText,
+                                        style: TextStyle(fontSize: 18, color: Colors.black),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            );
-                          }).toList(),
+                            ),
+                          );
+                        }).toList() ?? [],
+                      ),
+                    ),
+                    ScaleTransition(
+                      scale: _animationController.drive(
+                        Tween<double>(begin: 0.9, end: 1.0).chain(CurveTween(curve: Curves.easeInOut)),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: _selectedAnswers.isNotEmpty ? _nextQuestion : null,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 60),
+                          backgroundColor: Color(0xFF098EAB),
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          foregroundColor: Colors.white,
+                        ),
+                        child: Text(
+                          _currentQuestionIndex < (_quizCategory?.questions.length ?? 0) - 1
+                              ? 'Next'
+                              : 'Finish',
                         ),
                       ),
-                      ScaleTransition(
-                        scale: _animationController.drive(
-                          Tween<double>(begin: 0.9, end: 1.0).chain(CurveTween(curve: Curves.easeInOut)),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: _selectedAnswer != null ? _nextQuestion : null,
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 60),
-                            backgroundColor: Color(0xFF098EAB),
-                            padding: EdgeInsets.symmetric(vertical: 16),
-                            fixedSize: Size(double.infinity, 40),
-                            textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                            foregroundColor: Colors.white,
-                          ),
-                          child: Text(
-                            _currentQuestionIndex < _quizCategory!.questions.length - 1
-                                ? 'Next'
-                                : 'Finish',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
